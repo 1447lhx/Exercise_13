cff059e530b02e98d05f836a73c7e5df21c766ea
1447lhx <1959142791@qq.com>
2016-09-12 09:33:47 +0800

    update

--- app/src/main/java/com/example/dreamhigh/exercise_13/MainActivity.java
+++ app/src/main/java/com/example/dreamhigh/exercise_13/MainActivity.java
@@ -10,10 +10,8 @@ import android.view.View;
   import android.widget.TextView;
  import android.widget.Toast;
 
-
  public class MainActivity extends AppCompatActivity {
      public void onCreate(Bundle savedInstanceState) {
          super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_main);
-
      }}
